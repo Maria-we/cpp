ee61cdf9edbc2e5cf2c7522924624c1ffb704180
Maria Navasardyan <maria.navasardyan@basic.local>
2021-05-01 20:39:17 +0400

    ccout

---
+++ lesson2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+ int age;
+ int count;
+ //cout<< count;
+
+cout<<"Enter a number";
+cin>> count;
+
+cout<< "Enter a age";
+cin>>age;
+
+cout<<"count= "<<count<<endl;
+cout<<"age= "<<age;
+
+ return 0;
+}
